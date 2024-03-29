ae98f706fc79dafca32f8b36c533a11ee1b35f59
denyschr <denprogman99@gmail.com>
2024-03-29 16:43:28 +0200

    #1 refactor: remove the ng-skeleton component

--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -2,7 +2,6 @@ export * from './category';
 export * from './footer';
 export * from './header';
 export * from './header-auth';
-export * from './ng-skeleton';
 export * from './section';
 export * from './section-skeleton';
 export * from './sidebar';

1822b5086a61cbaa71b9c216fecab8f800570c9e
denyschr <denprogman99@gmail.com>
2024-04-05 20:44:18 +0300

    refactor(pipes): change the type of images from "any[]" to "Image[]"

--- src/app/shared/pipes/not-found-image/not-found-image.pipe.ts
+++ src/app/shared/pipes/not-found-image/not-found-image.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Image } from '../../../core/models';
 
 @Pipe({
   name: 'notFoundImage'
 })
 export class NotFoundImagePipe implements PipeTransform {
-  transform(images: any[]) {
+  transform(images: Image[]) {
     if (images.length && images[0]?.url) return images[0].url;
     else return 'assets/images/not-found.jpg';
   }

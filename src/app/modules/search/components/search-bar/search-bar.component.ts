d90867b8c2e9bb2caf71a6b57a2b6c4387fe45e0
denyschr <denprogman99@gmail.com>
2024-04-30 21:25:29 +0300

    refactor(search-bar): remove FormBuilder and add FormControl instead

--- src/app/modules/search/components/search-bar/search-bar.component.ts
+++ src/app/modules/search/components/search-bar/search-bar.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
+import { Subscription, debounceTime, distinctUntilChanged, tap } from 'rxjs';
 import { SearchService } from '@modules/search/services/search.service';
 import { MediaType } from '@models';
 
@@ -11,32 +11,36 @@ import { MediaType } from '@models';
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent implements OnInit, AfterViewInit, OnDestroy {
-  public searchControl = this._fb.nonNullable.group({
-    searchTerm: ''
-  });
-  public searchSubscription?: Subscription;
+  public searchControl = new FormControl('');
+  public searchTermSubscription?: Subscription;
   public initialPageLoading = true;
 
-  @ViewChild('searchInput') searchInput?: ElementRef<HTMLInputElement>;
+  @ViewChild('input') inputRef?: ElementRef<HTMLInputElement>;
 
   constructor(
-    private _fb: FormBuilder,
     private _searchService: SearchService,
     private _cdr: ChangeDetectorRef
   ) {
     this.searchControl.valueChanges
-      .pipe(debounceTime(300), distinctUntilChanged(), takeUntilDestroyed())
-      .subscribe((term) => {
-        this._searchService.setSearchTerm(term.searchTerm ?? '');
-        this._searchService.syncRouteParams();
-      });
+      .pipe(
+        takeUntilDestroyed(),
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap((term) => {
+          if (!term) this._searchService.searchType = MediaType.All;
+          this._searchService.searchTerm = term ?? '';
+          this._searchService.syncRouteParams();
+        })
+      )
+      .subscribe();
   }
 
   ngOnInit(): void {
-    this.searchSubscription = this._searchService.getSearchTerm().subscribe((term) => {
-      const currentTerm = this.searchControl.get('searchTerm')?.value;
+    this._searchService.searchType = MediaType.All;
+    this.searchTermSubscription = this._searchService.searchTerm.subscribe((term) => {
+      const currentTerm = this.searchControl.value;
       if (currentTerm !== term && !this.initialPageLoading) {
-        this.searchControl.get('searchTerm')?.setValue(term);
+        this.searchControl.setValue(term);
         this._cdr.detectChanges();
       }
       this.initialPageLoading = false;
@@ -44,19 +48,16 @@ export class SearchBarComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit(): void {
-    this.searchInput?.nativeElement.focus();
-  }
-
-  public clearSearchField(): void {
-    this._searchService.setSearchTerm('');
-    this._searchService.setSearchType(MediaType.All);
-    this.searchControl.get('searchTerm')?.setValue('');
-    this.searchInput?.nativeElement.focus();
+    this.inputRef?.nativeElement.focus();
   }
 
   ngOnDestroy(): void {
-    if (this.searchSubscription) {
-      this.searchSubscription.unsubscribe();
-    }
+    if (this.searchTermSubscription) this.searchTermSubscription.unsubscribe();
+  }
+
+  public clear(): void {
+    this._searchService.searchTerm = '';
+    this._searchService.searchType = MediaType.All;
+    this.inputRef?.nativeElement.focus();
   }
 }

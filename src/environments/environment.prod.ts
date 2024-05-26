a4a72a37896a1516b136be849851b4b12349ffb3
denyschr <denprogman99@gmail.com>
2024-05-26 12:14:57 +0300

    chore(environments): add spotify config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,22 @@
 export const environment = {
-  production: true,
+  production: true
+};
+
+export const SpotifyConfig = {
   apiUrl: 'https://api.spotify.com/v1',
-  tokenUrl: 'https://accounts.spotify.com/api/token'
+  tokenUrl: 'https://accounts.spotify.com/api/token',
+  redirectUri: 'https://denyschr.netlify.app/login',
+  authEndPoint: 'https://accounts.spotify.com/authorize',
+  scopes: [
+    'user-read-currently-playing',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-recently-played',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-library-read',
+    'user-library-modify',
+    'playlist-read-private',
+    'playlist-read-collaborative'
+  ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,22 @@
 export const environment = {
-  production: false,
+  production: false
+};
+
+export const SpotifyConfig = {
   apiUrl: 'https://api.spotify.com/v1',
-  tokenUrl: 'https://accounts.spotify.com/api/token'
+  tokenUrl: 'https://accounts.spotify.com/api/token',
+  redirectUri: 'http://localhost:4200/login',
+  authEndPoint: 'https://accounts.spotify.com/authorize',
+  scopes: [
+    'user-read-currently-playing',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-recently-played',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-library-read',
+    'user-library-modify',
+    'playlist-read-private',
+    'playlist-read-collaborative'
+  ]
 };

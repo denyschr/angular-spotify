3ca913d0e27b9561dddd0ce2c5b1659b94b318a3
denyschr <denprogman99@gmail.com>
2024-06-05 14:46:53 +0300

    feat(scss): update UI layout for search bar and footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MenuItem, SocialItem } from '@models';
 
 @Component({
   selector: 'app-footer',
@@ -8,78 +7,5 @@ import { MenuItem, SocialItem } from '@models';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {
-  public navTitle = 'Secondary navigation';
-
-  public companyMenuItems: MenuItem[] = [
-    {
-      name: 'About',
-      path: '/home'
-    },
-    {
-      name: 'Jobs',
-      path: '/home'
-    },
-    {
-      name: 'For the Record',
-      path: '/home'
-    }
-  ];
-
-  public communitiesMenuItems: MenuItem[] = [
-    {
-      name: 'For Artists',
-      path: '/home'
-    },
-    {
-      name: 'Developers',
-      path: '/home'
-    },
-    {
-      name: 'Advertising',
-      path: '/home'
-    },
-    {
-      name: 'Investors',
-      path: '/home'
-    },
-    {
-      name: 'Vendors',
-      path: '/home'
-    }
-  ];
-
-  public usefulLinksMenuItems: MenuItem[] = [
-    {
-      name: 'Support',
-      path: '/home'
-    },
-    {
-      name: 'Free Mobile App',
-      path: '/home'
-    }
-  ];
-
-  public extraMenuItems: MenuItem[] = [
-    { name: 'Legal', path: '/home' },
-    { name: 'Safety & Privacy Center', path: '/home' },
-    { name: 'Privacy Policy', path: '/home' },
-    { name: 'Cookies', path: '/home' },
-    { name: 'About Ads', path: '/home' },
-    { name: 'Accessibility', path: '/home' }
-  ];
-
-  public socialItems: SocialItem[] = [
-    {
-      path: '/home',
-      icon: '_icon-instagram'
-    },
-    {
-      path: '/home',
-      icon: '_icon-twitter'
-    },
-    {
-      path: '/home',
-      icon: '_icon-facebook'
-    }
-  ];
+  public currentYear = new Date().getFullYear();
 }

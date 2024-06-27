d63890636234527c171cdeb8ff7c4803bbadec22
denyschr <denprogman99@gmail.com>
2024-06-27 21:42:51 +0300

    feat: remove the displaying of user`s name as a greeting

--- src/app/modules/home/components/greeting/greeting.component.ts
+++ src/app/modules/home/components/greeting/greeting.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { UserService } from '@services';
 import { randomEmoji } from '@utils';
-import { map } from 'rxjs';
 
 @Component({
   selector: 'sp-greeting',
@@ -10,16 +8,10 @@ import { map } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GreetingComponent {
-  public readonly $message = this._userService
-    .getUserProfile()
-    .pipe(map(res => this.getMessage(res.display_name)));
-
-  constructor(private _userService: UserService) {}
-
-  private getMessage(name: string): string {
+  get message(): string {
     const now = new Date();
     const hours = now.getHours();
     const text = hours < 12 ? 'Morning 🌞' : hours < 18 ? 'Afternoon 🌤' : 'Evening 🌙';
-    return `Good ${text} ${randomEmoji()} ${name}`;
+    return `Good ${text} ${randomEmoji()}`;
   }
 }

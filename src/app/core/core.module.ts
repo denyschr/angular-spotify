4fbd1e9c37fc28b5d060fc024cc3b6a17b233e62
denyschr <denprogman99@gmail.com>
2024-06-27 21:37:54 +0300

    chore: remove unnecessary imported modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,32 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StandardPageLayoutComponent, LeftSidebarComponent } from '@components';
 import { RouterModule } from '@angular/router';
-import {
-  AlbumModule,
-  ArtistModule,
-  HomeModule,
-  LibraryModule,
-  NotFoundModule,
-  PlaylistModule,
-  SearchModule,
-  TrackModule
-} from '@modules';
 import { OpacityScrollModule } from '@directives';
 
 @NgModule({
   declarations: [StandardPageLayoutComponent, LeftSidebarComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    HomeModule,
-    SearchModule,
-    LibraryModule,
-    AlbumModule,
-    ArtistModule,
-    PlaylistModule,
-    TrackModule,
-    OpacityScrollModule,
-    NotFoundModule
-  ]
+  imports: [CommonModule, RouterModule, OpacityScrollModule]
 })
 export class CoreModule {}

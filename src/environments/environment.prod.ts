b874ef2ad6cb41386d37cbd1e86b8915248da7bf
denyschr <denprogman99@gmail.com>
2024-07-03 15:46:56 +0300

    chore(environments): change redirect url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const SpotifyConfig = {
   clientId: 'd49c038f96234ecf946b2bab56d6a127',
   apiUrl: 'https://api.spotify.com/v1',
   tokenUrl: 'https://accounts.spotify.com/api/token',
-  redirectUrl: 'https://denyschr.netlify.app/login',
+  redirectUrl: 'https://angular-spotify-denyschr.netlify.app/login',
   authEndPoint: 'https://accounts.spotify.com/authorize',
   scopes: ['user-read-recently-played', 'user-top-read', 'user-library-read', 'user-read-private']
 };
